c268a5981f4c998530ec22c0abb431f0601eed98
Amber Hunt <amberhunt955@gmail.com>
2023-06-11 22:47:52 -0400

    all route frameworks set

---
+++ api/controllers/api/auth.js
@@ -0,0 +1,5 @@
+const auth = (req, res) => {
+    res.json("HELLO FROM CONTROLLER AUTH")
+}
+
+module.exports = { auth }
--- api/index.js
+++ api/index.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json())
 
 // routes
+app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
 
 //* LISTEN AT DESIGNATED PORT
 const PORT = process.env.PORT | 3001;
